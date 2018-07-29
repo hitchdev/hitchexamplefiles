af65753c265611951e99dfa9fe0ad3bc329c8ef3
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-07-29 19:33:22 +0100

    BUGFIX : Fix imports and added hitch.

--- hitchexamplefiles/__init__.py
+++ hitchexamplefiles/__init__.py
@@ -2,7 +2,11 @@ from path import Path
 
 THIS_DIRECTORY = Path(__file__).realpath().dirname()
 
+
 jpeg_large = THIS_DIRECTORY / "jpeg_large.jpg"
 jpeg_small = THIS_DIRECTORY / "jpeg_small.jpg"
 pdf_one_page = THIS_DIRECTORY / "pdf_one_page.pdf"
 png_large = THIS_DIRECTORY / "png_large.pdf"
+
+
+__VERSION__ = "DEVELOPMENT_VERSION"
